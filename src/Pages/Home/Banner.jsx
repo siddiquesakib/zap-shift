1f4c1b2ceee0084ef9930d3f9dba8601659b8ed7
tahrim sakib <dmsakib1122@gmail.com>
2025-11-16 19:59:13 +0600

    first commit

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <figure>
+            <img src="../../assets/tiny-deliveryman.png" alt="img" />
+          </figure>
+        </div>
+        <div>
+          <h1>We Make Sure Your Parcel Arrives On Time – No Fuss.</h1>
+          <p>
+            Enjoy fast, reliable parcel delivery with real-time tracking and
+            zero hassle. From personal packages to business shipments — we
+            deliver on time, every time.
+          </p>
+        </div>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default Banner;
